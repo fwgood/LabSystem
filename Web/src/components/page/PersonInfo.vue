5136af5f15af5ad8af3e55a09fa2d5794b78d7dd
fwgood <fwgood@qq.com>
2017-12-16 23:29:09 +0800

    信息部分修改

--- Web/src/components/page/PersonInfo.vue
+++ Web/src/components/page/PersonInfo.vue
@@ -7,38 +7,34 @@
       </el-breadcrumb>
     </div>
     <el-card class="box-card">
-      <div slot="header" class="clearfix">
-          <div style="padding-right:10px">
+  <div slot="header" class="clearfix">
+              <div style="padding-right:10px">
             <strong>姓名：{{user.username}}</strong>
           </div>
           <div style="padding-right:10px">
             <strong>学号：{{user.account}}</strong>
           </div>
-      </div>
-      <div class="head-right">
-        <el-button type="primary" @click="editDialog = true">
-          <i class="el-icon-edit"></i>
-        </el-button>
-      </div>
-      <div>
+    <el-button style="float: right; padding: 3px 0;margin:-30px 0 0 0" type="text" @click="editDialog = true">操作按钮</el-button>
+  </div>
+<div>
         <div class="headImg" style="display:inline-block;">
-          <img src="../../assets/head.jpg" alt="sorry" width="200px;">
+          <img src="../../assets/head.jpg" alt="sorry" width="200px;" > 
         </div>
         <div class="personal-info" style="display:inline-block;">
-          <div>国家和地区：{{user.country}}</div>
-          <div>市/县：{{user.city}}</div>
-          <div>Email：{{user.email}}</div>
-          <div>专业：{{user.major}}</div>
+          <div>年级：{{user.country}}</div>
+          <div>学院：{{user.city}}</div>
+          <div>专业：{{user.email}}</div>
+          <div>电话：{{user.major}}</div>
           <div>课程档案：
             <div v-for="item in user.course" :key="item.id" style="margin-right:10px;display:inline-block;">
               <router-link :to="item.to">{{item.courseName}}</router-link>
             </div>
+            <el-tag>标签一</el-tag>
           </div>
-          <div>首次访问：{{user.firstVisit}}</div>
+          <div>QQ：{{user.firstVisit}}</div>
           <div>上次访问：{{user.lastVisit}}</div>
-        </div>
-      </div>
-    </el-card>
+        </div></div>
+</el-card>
     <el-dialog title="编辑资料" :visible.sync="editDialog">
       <el-form :model="info">
         <el-form-item label="姓名" label-width="100px">
@@ -47,54 +43,36 @@
         <el-form-item label="学号" label-width="100px">
           <el-input v-model="info.account" auto-complete="off" :disabled="true" :placeholder="user.account"></el-input>
         </el-form-item>
-        <el-form-item label="市区" label-width="100px">
-          <el-input v-model="info.city" auto-complete="off" :disabled="true" :placeholder="user.city"></el-input>
-        </el-form-item>
-        <el-form-item label="国家或地区" label-width="100px">
-          <el-input v-model="info.country" auto-complete="off" :disabled="true" :placeholder="user.country"></el-input>
-        </el-form-item>
         <el-form-item label="年级" label-width="100px">
-          <el-input v-model="info.grade" auto-complete="off"></el-input>
+          <el-input v-model="info.grade" auto-complete="off" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="学院" label-width="100px">
+          <el-input v-model="info.academy" auto-complete="off" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="专业" label-width="100px">
+          <el-input v-model="info.major" auto-complete="off" disabled></el-input>
         </el-form-item>
         <el-form-item label="手机" label-width="100px">
           <el-input v-model="info.phone" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="学院" label-width="100px">
-          <el-input v-model="info.academy" auto-complete="off"></el-input>
+        <el-form-item label="QQ" label-width="100px">
+          <el-input v-model="info.qq" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="专业" label-width="100px">
-          <el-input v-model="info.major" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="自述" label-width="100px">
-          <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="info.intro"></el-input>
-        </el-form-item>
-        <el-form-item label="兴趣列表" label-width="100px">
-          <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="info.habby"></el-input>
-        </el-form-item>
-        <el-form-item label="网页" label-width="100px">
-          <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="info.page"></el-input>
-        </el-form-item>
-        <el-form-item label="QQ号码" label-width="100px">
-          <el-input placeholder="请输入QQ" v-model="info.qq"></el-input>
-        </el-form-item>
-        <el-form-item label="Skype号码" label-width="100px">
-          <el-input placeholder="请输入Skype" v-model="info.skype"></el-input>
-        </el-form-item>
-        <el-form-item label="Yahoo号码" label-width="100px">
-          <el-input placeholder="请输入Yahoo" v-model="info.yahoo"></el-input>
-        </el-form-item>
-        <el-form-item label="MSN号码" label-width="100px">
-          <el-input placeholder="请输入MSN" v-model="info.msn"></el-input>
-        </el-form-item>
-        <el-form-item label="卡号" label-width="100px">
-          <el-input placeholder="请输入内容" v-model="info.cardNumber"></el-input>
-        </el-form-item>
+
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="editDialog = false">取消</el-button>
         <el-button type="primary" @click="updateInfo">确定</el-button>
       </div>
     </el-dialog>
+    <el-popover
+  ref="popover1"
+  placement="top-start"
+  title="标题"
+  width="200"
+  trigger="hover"
+  content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+</el-popover>
   </div>
 </template>
 <script>
